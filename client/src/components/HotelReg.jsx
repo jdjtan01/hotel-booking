82452bf222422b6337ef71dafdebaf77f16d0492
jdjtan <jdjtan01@gmail.com>
2025-07-03 20:07:00 +0800

    Finishing frontend, connected frontend with backend

--- client/src/components/HotelReg.jsx
+++ client/src/components/HotelReg.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets, cities } from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
+import toast from 'react-hot-toast';
 
 const HotelReg = () => {
+
+    const {setShowHotelReg, axios, getToken, setIsOwner} = useAppContext();
+
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [contact, setContact] = useState('');
+    const [city, setCity] = useState('');
+
+    const onSubmitHandler = async (event) => {
+        event.preventDefault();
+        try {
+            const {data} = await axios.post(`/api/hotels/`, {name, contact, address, city}, {headers: {Authorization: `Bearer ${await getToken()}`}});
+
+            if(data.success) {
+                toast.success(data.message);
+                setIsOwner(true);
+                setShowHotelReg(false);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+    
   return (
-    <div className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70'>
-        <form className='flex bg-white rounded-xl max-w-4xl max-md:mx-2'>
+    <div onClick={() => setShowHotelReg(false)} className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/70'>
+        <form onSubmit={onSubmitHandler} onClick={event => event.stopPropagation()} className='flex bg-white rounded-xl max-w-4xl max-md:mx-2'>
             <img src={assets.regImage} alt="reg_img" className='w-1/2 rounded-xl hidden md:block' />
 
             <div className='relative flex flex-col items-center md:w-1/2 p-8 md:p-10'>
-                <img src={assets.closeIcon} alt="close_icon" className='absolute top-4 right-4 h-4 w-4 cursor-pointer' />
+                <img src={assets.closeIcon} alt="close_icon" className='absolute top-4 right-4 h-4 w-4 cursor-pointer' onClick={() => setShowHotelReg(false)} />
                 <p className='text-2xl font-semibold mt-6'>Register Your Hotel</p>
 
                 {/* Hotel Name */}
@@ -16,7 +43,7 @@ const HotelReg = () => {
                     <label htmlFor="name" className='font-medium text-gray-500'>
                         Hotel Name
                     </label>
-                    <input type="text" id="name" placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required/>
+                    <input type="text" id="name" onChange={(event) => setName(event.target.value)} value={name} placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required/>
                 </div>
 
                 {/* Phone number */}
@@ -24,7 +51,7 @@ const HotelReg = () => {
                     <label htmlFor="contact" className='font-medium text-gray-500'>
                         Contact No.
                     </label>
-                    <input type="text" id="contact" placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required/>
+                    <input type="text" id="contact" onChange={(event) => setContact(event.target.value)} value={contact} placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required/>
                 </div>
 
                 {/* Address */}
@@ -32,13 +59,13 @@ const HotelReg = () => {
                     <label htmlFor="address" className='font-medium text-gray-500'>
                         Address
                     </label>
-                    <input type="text" id="address" placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required/>
+                    <input type="text" id="address" onChange={(event) => setAddress(event.target.value)} value={address} placeholder='Type here' className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required/>
                 </div>
 
                 {/* Select City Drop down */}
                 <div className='w-full mt-4 max-w-60 mr-auto'>
                     <label htmlFor="city" className='font-medium text-gray-500'>City</label>
-                    <select id="city" className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required>
+                    <select id="city" onChange={(event) => setCity(event.target.value)} value={city} className='border border-gray-200 rounded w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light' required>
                         <option value="">Select City</option>
                         {cities.map((city) => (
                             <option key={city} value={city}>{city}</option>
--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import Title from '../../components/Title'
 import { assets } from '../../assets/assets'
+import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast'
 
 const AddRoom = () => {
+
+  const {axios, getToken,} = useAppContext();
 
   const [images, setImages] = useState({
     1: null,
@@ -15,17 +19,73 @@ const AddRoom = () => {
     roomType: '',
     pricePerNight: 0,
     amenities: {
-      'Free Wifi': false,
+      'Free WiFi': false,
       'Free Breakfast': false,
       'Room Service': false,
       'Mountain View': false,
       'Pool Access': false,
-      'Gym Access': false,
     }
   })
 
+  const [loading, setLoading] = useState(false);
+
+  const onSubmitHandler = async () => {
+    event.preventDefault();
+    // Check if all inputs are filled
+    if(!inputs.roomType || !inputs.pricePerNight || !inputs.amenities || !Object.values(images).some(image => image)) {
+      toast.error("Please fill-in all the details");
+      return;
+    }
+    setLoading(true);
+    try {
+      const formData = new FormData();
+      formData.append('roomType', inputs.roomType);
+      formData.append('pricePerNight', inputs.pricePerNight);
+      // Converting amenities to array & keeping only enabled amenities
+      const amenities = Object.keys(inputs.amenities).filter(key => inputs.amenities[key]);
+      formData.append('amenities', JSON.stringify(amenities));
+
+      // Adding images to form data
+      Object.keys(images).forEach((key) => {
+        images[key] && formData.append('images', images[key])
+      });
+
+      const { data } = await axios.post('/api/rooms', formData, {headers: {Authorization: `Bearer ${await getToken()}`}});
+
+      if(data.success) {
+        toast.success(data.message)
+        setInputs({
+          roomType: '',
+          pricePerNight: 0,
+          amenities: {
+            'Free WiFi': false,
+            'Free Breakfast': false,
+            'Room Service': false,
+            'Mountain View': false,
+            'Pool Access': false,
+          }
+        });
+          setImages({
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+          });
+      } else {
+        toast.error(data.message);
+      }
+
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+
+
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
 
       <Title title="Add Room" subTitle="Fill in the details carefully with accurate room details, pricing, and amenities to enhance the user booking experience." align='left' font='outfit' />
 
@@ -71,7 +131,9 @@ const AddRoom = () => {
         ))}
       </div>
 
-      <button className='bg-primary text-white px-8 py-2 rounded mt-8 cursor-pointer'>Add Room</button>
+      <button className='bg-primary text-white px-8 py-2 rounded mt-8 cursor-pointer' disabled={loading}>
+        {loading? 'Adding...' : 'Add Room'}
+      </button>
 
     </form>
   )
